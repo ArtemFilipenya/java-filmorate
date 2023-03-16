d7342ae2773b1bf46da0213d43e2fffbda9d7227
Artem Filipenya <as.filipenya@gmail.com>
2023-03-16 21:04:36 +0300

    v8.0: refactor Genre

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
@@ -51,43 +50,22 @@ public class UserService {
     }
 
     public void addFriend(Integer userId, Integer friendId) throws NotFoundException {
-//        if (userId <=0 || friendId <= 0) {
-//            throw new NotFoundException("id and friendId cannot be less 0");
-//        }
         users.addFriend(userId, friendId);
     }
 
     public void deleteFriend(Integer userId, Integer friendId) throws ValidateException {
-//        if (userId <=0 || friendId <= 0) {
-//            throw new ValidateException("id and friendId cannot be less 0");
-//        }
-//        if (userId.equals(friendId)) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You cant delete yourself");
-//        }
         users.deleteFriend(userId, friendId);
     }
 
     public List<User> getCommonFriends(Integer userId, Integer friendId) throws ValidateException {
-//        if (userId <=0 || friendId <= 0) {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "id and friendId cannot be less 0");
-//        }
-//        if (userId.equals(friendId)) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to request mutual friends of self");
-//        }
         return  users.getCommonFriends(userId, friendId);
     }
 
     public List<User> getFriends(Integer friendId) throws ValidateException {
-//        if (friendId <=0 ) {
-//            throw new ValidateException("id and friendId cannot be less 0");
-//        }
         return users.getFriends(friendId);
     }
 
     public User getUser(Integer userId) {
-//        if (userId <= 0) {
-//            throw new ValidateException("id and friendId cannot be less 0");
-//        }
         return users.getUser(userId);
     }
 }
